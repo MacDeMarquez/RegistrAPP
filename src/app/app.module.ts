26fdbdb8a4c2783a399d06c899eff66919790840
INFFame <ma.garcial@duocuc.cl>
2023-09-20 18:52:03 -0300

    Arreglando angular material
    arreglando error de imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'; //para utilizar Angular Material
-import { MatSelectModule } from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MatSlideToggleModule, MatSelectModule, MatFormFieldModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
